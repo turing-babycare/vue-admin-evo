8465169d0b3f7297d995d808dbe2474694565159
Allen Iversion <372452506@qq.com>
2022-08-25 10:45:00 +0800

    fix: 修改token存储位置

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,15 @@
 const TOKEN_STORE_KEY = '_token';
 export function getToken() {
-  return window.localStorage.getItem(TOKEN_STORE_KEY);
+  // return window.localStorage.getItem(TOKEN_STORE_KEY);
+  return window.document.cookie.split(`${TOKEN_STORE_KEY}=`)[1];
 }
 
 export function setToken(v: string) {
-  return window.localStorage.setItem(TOKEN_STORE_KEY, v);
+  // return window.localStorage.setItem(TOKEN_STORE_KEY, v);
+  return (window.document.cookie = `${TOKEN_STORE_KEY}=${v}`);
 }
 
 export function removeToken() {
-  return window.localStorage.removeItem(TOKEN_STORE_KEY);
+  // return window.localStorage.removeItem(TOKEN_STORE_KEY);
+  return (window.document.cookie = '');
 }
